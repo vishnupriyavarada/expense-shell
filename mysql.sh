8be9040f4c2bb7c313413156304d642fd87a790b
vishnupriyavarada <vvarada09@gmail.com>
2025-07-13 12:28:14 +0530

    mysql.sh initial code checkin

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shellscript-log"
+LOG_FILE= $(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date '+%Y-%m-%d-%H-%M-%S')
+LOGFILE="${LOGS_FOLDER}/${LOG_FILE}-${TIME_STAMP}.log"
+
+echo " $(0) started executing at ${TIME_STAMP}" &>>${LOGFILE}
+
+if [ ${USERID} !=  0 ]
+then
+    echo "ERROR: You must have sudo/root user access to execute this script" &>>${LOGFILE}
+fi
+
+
+    
---
+++ readme.md
@@ -0,0 +1,1 @@
+*** Expense project automating with Shell script

bf183b4491c8faaea489292811a392a154b58f53
vishnupriyavarada <vvarada09@gmail.com>
2025-07-13 18:22:36 +0530

    backend.sh and backend.service initial code checkin

--- backend.sh
+++ backend.sh
@@ -29,9 +29,9 @@ CHECK_ROOT_USER
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 ... success" >>&${LOGFILE}
+        echo "$2 ... success" &>>${LOGFILE}
     else
-        echo "$2 ... failed" >>&${LOGFILE}
+        echo "$2 ... failed" &>>${LOGFILE}
     fi
 }
 
@@ -39,16 +39,16 @@ NODEJS_STATUS=$(dnf list available | grep nodejs)
 
 if[ NODEJS_STATUS -eq 0 ]
 then
-    dnf module disable nodejs -y >>&${LOGFILE}
+    dnf module disable nodejs -y &>>${LOGFILE}
     VALIDATE $? "Disabling older nodejs"
 
-    dnf module enable nodejs:20 -y >>&${LOGFILE}
+    dnf module enable nodejs:20 -y &>>${LOGFILE}
     VALIDATE $? "Enabling nodejs:20"
 
-    dnf install nodejs -y >>&${LOGFILE}
-    VALIDATE $? "Installing nodejs:20" >>&${LOGFILE}
+    dnf install nodejs -y &>>${LOGFILE}
+    VALIDATE $? "Installing nodejs:20" &>>${LOGFILE}
 else
-    dnf install nodejs -y >>&${LOGFILE}
+    dnf install nodejs -y &>>${LOGFILE}
     VALIDATE $? "Installing nodejs:20" 
 fi
 
@@ -61,33 +61,33 @@ then
     VALIDATE $? "Creating app directory"   
 fi
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip >>&${LOGFILE}
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>${LOGFILE}
 VALIDATE $? "Downloading backend files"  
 
 cd /app
 
-unzip /tmp/backend.zip >>&${LOGFILE}
+unzip /tmp/backend.zip &>>${LOGFILE}
 VALIDATE $? "unzip backend files"  
 
-npm install >>&${LOGFILE}
+npm install &>>${LOGFILE}
 VALIDATE $? "install dependencies"  
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
 
 # Prepare mysql schema
-dnf install mysql -y  >>&${LOGFILE}
+dnf install mysql -y  &>>${LOGFILE}
 VALIDATE $? "Installing mysql client"  
 
 mysql -h mysql.vishnudevopsaws.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 VALIDATE $? "Setting up the transactions schema and tables"  
 
-systemctl daemon-reload >>&${LOGFILE}
+systemctl daemon-reload &>>${LOGFILE}
 VALIDATE $? "Daemon reload" 
 
-systemctl enable backend >>&${LOGFILE}
+systemctl enable backend &>>${LOGFILE}
 VALIDATE $? "Enabling backend" 
 
-systemctl start backend >>&${LOGFILE}
+systemctl start backend &>>${LOGFILE}
 VALIDATE $? "Starting backend" 
 

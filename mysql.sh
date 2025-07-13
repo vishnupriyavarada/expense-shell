be7479bdf8e13bb1b955c1e03d4d8aa44ab62d0a
vishnupriyavarada <vvarada09@gmail.com>
2025-07-13 12:57:43 +0530

    mysql.sh initial code checkin

--- mysql.sh
+++ mysql.sh
@@ -2,17 +2,30 @@
 
 USERID=$(id -u)
 
-LOGS_FOLDER="/var/log/shellscript-log"
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE= $(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date '+%Y-%m-%d-%H-%M-%S')
 LOGFILE="${LOGS_FOLDER}/${LOG_FILE}-${TIME_STAMP}.log"
 
+if [ ! -d ${LOGFILE} ]
+then
+    echo "Log directory does not exist.Creating log directory..."
+    mkdir -p ${LOGFILE}
+fi
+
+
+CHEK_ROOT_USER(){
+    if [ ${USERID} !=  0 ]
+    then
+        echo "ERROR: You must have sudo/root user access to execute this script" &>>${LOGFILE}
+        exit 1 #other than 0
+    fi
+
+}
+
 echo "$0 started executing at ${TIME_STAMP}" &>>${LOGFILE}
 
-if [ ${USERID} !=  0 ]
-then
-    echo "ERROR: You must have sudo/root user access to execute this script" &>>${LOGFILE}
-fi
+
 
 
     
